e895d2fb7ca49fccee032c4a4c06cfcc3c064059
Shawn DelPercio <sdelperc@gmail.com>
2020-02-10 15:15:27 -0500

    added todolist and todoform into app.js

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h2>Todo Form</h2>
+        );
+    }
+}
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h2>Todo List</h2>
+        );
+    }
+}
+export default TodoList;
